d41ac9c9170e8be57702567e3645ca92c97d6d74
Ritalin <kazu.ktz.tamura@gmail.com>
2025-05-09 17:43:27 +0900

    Add test for generaring keywordhash header

--- crates/tool/build-support/src/lib.rs
+++ crates/tool/build-support/src/lib.rs
@@ -12,7 +12,8 @@ pub fn download_sqlite_repository(build_dir: &PathBuf) -> Result<PathBuf, anyhow
     if ! dest_dir.exists() {
         let path_str = dest_dir.display().to_string();
         std::process::Command::new("git")
-            .args(&["clone", "--depth=1", "--branch=master", "https://github.com/sqlite/sqlite.git", &path_str])
+            .arg("clone")
+            .args(&["--depth=1", "--branch=master", "https://github.com/sqlite/sqlite.git", &path_str])
             .stdout(std::process::Stdio::inherit())
             .stderr(std::process::Stdio::inherit())
             .output()?
@@ -20,6 +21,59 @@ pub fn download_sqlite_repository(build_dir: &PathBuf) -> Result<PathBuf, anyhow
     }
 
     Ok(dest_dir)
+}
+
+pub fn generate_keywordhash(build_dir: &PathBuf, sqlite_dir: &PathBuf) -> Result<PathBuf, anyhow::Error> {
+    let generated_command = generate_keywordhash_header(build_dir, sqlite_dir)?;
+    
+    run_mkkeywordhash(&build_dir, &generated_command)
+}
+
+fn run_mkkeywordhash(build_dir: &PathBuf, command_path: &PathBuf) -> Result<PathBuf, anyhow::Error> {
+    let out_file_path = build_dir.join("keywordhash.h");
+    run_command_with_redirect(&command_path.display().to_string(), Some(out_file_path.display().to_string()))?;
+
+    Ok(out_file_path)
+}
+
+fn generate_keywordhash_header(build_dir: &PathBuf, sqlite_dir: &PathBuf) -> Result<PathBuf, anyhow::Error> {
+    let source_path = sqlite_dir.join("tool/mkkeywordhash.c");
+    let out_file_path = build_dir.join("mkkeywordhash");
+
+    run_build_c(&source_path.display().to_string(), &out_file_path.display().to_string())?;
+    Ok(out_file_path)
+}
+
+fn run_build_c(source_path: &str, out_path: &str) -> Result<(), anyhow::Error> {
+    eprintln!("run_build_c called !");
+    println!("cargo:rerun-if-changed={source_path}");
+
+    let status = std::process::Command::new("zig")
+        .arg("cc")
+        .args(&[source_path, "-o", out_path])
+        .args(&["-o", out_path])
+        .status()
+        .map_err(|_| anyhow::anyhow!("Failed to compile {source_path}"))?
+    ;
+    assert!(status.success(), "Clang compilation failed");
+
+    Ok(())
+}
+
+fn run_command_with_redirect(command_path: &str, redirect_path: Option<String>) -> Result<(), anyhow::Error> {
+    let mut command = std::process::Command::new(&command_path);
+
+    if let Some(redirect_path) = redirect_path.as_ref() {
+        let file = std::fs::File::create(redirect_path).map_err(|_| anyhow::anyhow!("Failed to create {redirect_path}"))?;
+        command.stdout(file);
+    }
+
+    let mut child = command.spawn().map_err(|_| anyhow::anyhow!("Failed to execute {command_path}"))?;
+    let status = child.wait().map_err(|_| anyhow::anyhow!("Failed to wait for gen_code"))?;
+
+    assert!(status.success(), "{command_path} execution failed");
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -31,6 +85,18 @@ mod build_tests {
         let dest_dir = download_sqlite_repository(&get_tool_build_dir()?)?;
 
         assert!(dest_dir.join(".git").exists());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_generate_keywordhash() -> Result<(), anyhow::Error> {
+        let build_dir = get_tool_build_dir()?;
+        let command_path = generate_keywordhash_header(&build_dir, &build_dir.join("sqlite"))?;
+        assert!(command_path.exists());
+
+        let file_path = run_mkkeywordhash(&build_dir, &command_path)?;
+        assert!(file_path.exists());
 
         Ok(())
     }
