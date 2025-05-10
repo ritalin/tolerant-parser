4add4b665808c158958b01326582c7f779ef2798
Ritalin <kazu.ktz.tamura@gmail.com>
2025-05-10 16:48:39 +0900

    Add scanner rule json

--- crates/tool/extract_lemon_grammar/src/main.rs
+++ crates/tool/extract_lemon_grammar/src/main.rs
@@ -1,8 +1,8 @@
 use std::path::PathBuf;
 
 pub fn main() -> Result<(), anyhow::Error> {
-    let manifest_dir = env!("CARGO_MANIFEST_DIR");
-    let build_dir = PathBuf::from(manifest_dir).join("../../../build");
+    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let build_dir = manifest_dir.join("../../../build");
     let grammar_file_path = build_dir.join("sqlite/src/parse.y").display().to_string();
 
     let builder = extract_lemon_grammar::lemon::LemonBuilder::new();
@@ -13,16 +13,25 @@ pub fn main() -> Result<(), anyhow::Error> {
     ;
 
     lemon.parse();
-    let symbols = lemon.symbols();
-    let grammar_file_path = build_dir.join("grammar_symbols.json");
 
-    std::fs::write(grammar_file_path, serde_json::to_string_pretty(&symbols)?)?;
+    'export_symbol: {
+        let symbols = lemon.symbols();
+        let grammar_file_path = build_dir.join("grammar_symbols.json");
 
-    let rules = lemon.rules();
-    let grammar_file_path = build_dir.join("grammar_rules.json");
+        std::fs::write(grammar_file_path, serde_json::to_string_pretty(&symbols)?)?;
+        break 'export_symbol;
+    }
+    'export_parsing_rule: {
+        let rules = lemon.rules();
+        let grammar_file_path = build_dir.join("grammar_parse_rules.json");
 
-    std::fs::write(grammar_file_path, serde_json::to_string_pretty(&rules)?)?;
-
+        std::fs::write(grammar_file_path, serde_json::to_string_pretty(&rules)?)?;
+        break 'export_parsing_rule;
+    }
+    'export_scanning_rule: {
+        std::fs::copy(manifest_dir.join("src/assets/scan_rules.json"), build_dir.join("grammar_scan_rules.json"))?;
+        break 'export_scanning_rule;
+    }
     Ok(())
 }
 
