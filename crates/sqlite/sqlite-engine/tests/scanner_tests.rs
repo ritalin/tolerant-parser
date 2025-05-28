c4a58cf833b0852431892ff6a3ca58b172bdee17
Ritalin <kazu.ktz.tamura@gmail.com>
2025-05-29 01:20:24 +0900

    Update sqlite engine

--- crates/sqlite/sqlite-engine/tests/scanner_tests.rs
+++ crates/sqlite/sqlite-engine/tests/scanner_tests.rs
@@ -161,6 +161,82 @@ fn test_shift_with_leading_trivia() -> Result<(), anyhow::Error> {
 }
 
 #[test]
+fn test_has_invalid_token() -> Result<(), anyhow::Error> {
+    let source = "SELECT 1 } FROM foo";
+    let engine = sqlite_engine::create()?;
+    let mut scanner = Scanner::create(source, 0, engine.scanning_rules)?;
+
+    'scanning: {
+        let expect_token = Token {
+            leading_trivia: None,
+            main: ScanEvent { kind: syntax_kind::r#SELECT, offset: 0, len: 6, value: Some("SELECT".into()) },
+            trailing_trivia: Some(vec![
+                ScanEvent { kind: syntax_kind::r#SPACE, offset: 6, len: 1, value: Some(" ".into()) }
+            ]),
+        };
+        assert_eq!(Some(expect_token), scanner.shift());
+        break 'scanning;
+    }
+    'scanning: {
+        let expect_token = Token {
+            leading_trivia: None,
+            main: ScanEvent { kind: syntax_kind::r#INTEGER, offset: 7, len: 1, value: Some("1".into()) },
+            trailing_trivia: Some(vec![
+                ScanEvent { kind: syntax_kind::r#SPACE, offset: 8, len: 1, value: Some(" ".into()) }
+            ]),
+        };
+        assert_eq!(Some(expect_token), scanner.shift());
+        break 'scanning;
+    }
+    'scanning: {
+        let expect_token = Token {
+            leading_trivia: None,
+            main: ScanEvent { kind: syntax_kind::ILLEGAL, offset: 9, len: 1, value: Some("}".into()) },
+            trailing_trivia: Some(vec![
+                ScanEvent { kind: syntax_kind::r#SPACE, offset: 10, len: 1, value: Some(" ".into()) }
+            ]),
+        };
+        assert_eq!(Some(expect_token), scanner.shift());
+        break 'scanning;
+    }
+    'scanning: {
+        let expect_token = Token {
+            leading_trivia: None,
+            main: ScanEvent { kind: syntax_kind::FROM, offset: 11, len: 4, value: Some("FROM".into()) },
+            trailing_trivia: Some(vec![
+                ScanEvent { kind: syntax_kind::r#SPACE, offset: 15, len: 1, value: Some(" ".into()) }
+            ]),
+        };
+        assert_eq!(Some(expect_token), scanner.shift());
+        break 'scanning;
+    }
+    'scanning: {
+        let expect_token = Token {
+            leading_trivia: None,
+            main: ScanEvent { kind: syntax_kind::ID, offset: 16, len: 3, value: Some("foo".into()) },
+            trailing_trivia: None,
+        };
+        assert_eq!(Some(expect_token), scanner.shift());
+        break 'scanning;
+    }
+    'scanning: {
+        let expect_token = Token {
+            leading_trivia: None,
+            main: ScanEvent { kind: syntax_kind::EOF, offset: 19, len: 0, value: None },
+            trailing_trivia: None,
+        };
+        assert_eq!(Some(expect_token), scanner.shift());
+        break 'scanning;
+    }
+    'scanning: {
+        assert_eq!(None, scanner.shift());
+        break 'scanning;
+    }
+
+    Ok(())
+}
+
+#[test]
 fn test_scan_scope() -> Result<(), anyhow::Error> {
     let source = "INSERT OR REPLACE  INTO";
     let engine = sqlite_engine::create()?;
