850601a759316e399ab37631156f627aa65f7d04
Ritalin <kazu.ktz.tamura@gmail.com>
2025-08-01 12:13:11 +0900

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,12 @@ pub mod core {
 #[cfg(feature = "wasi")]
 pub mod wasi {
     pub mod parser_wasi;
+    pub mod event_capture_wasi;
 }
 #[cfg(feature = "wasi")]
 pub use crate::__export_parser_world_impl as export_parser;
+#[cfg(feature = "wasi")]
+pub use crate::__export_event_capture_world_impl as export_capture;
 
 pub use core::engine_core::SyntaxKind;
 
